6632bb3788d377b75bdefdf4a383ad4c0fe4a6a3
Jose Tafur <j-osea-nt@hotmail.com>
2019-08-12 20:04:49 -0500

    adding course type

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
-const { GraphQLSchema, GraphQLObjectType, GraphQLString, graphql } = require('graphql');
+const { GraphQLSchema, GraphQLObjectType, GraphQLString, graphql, GraphQLInt } = require('graphql');
 
 const app = express();
+
+const courseType = new GraphQLObjectType({
+  name: 'Course',
+  fields: {
+    title: { type: GraphQLString },
+    views: { type: GraphQLInt }
+  }
+});
 
 // definir el schema
 // la funcion resolve indica como se debe responder cuando se solicite la prop message
@@ -14,6 +22,12 @@ const schema = new GraphQLSchema({
         resolve(){
           return "Hola"
         }
+      },
+      course: {
+        type: courseType,
+        resolve() {
+          return { title: 'Course of GraphQL', views: 1850 };
+        }
       }
     }
   })
@@ -21,7 +35,7 @@ const schema = new GraphQLSchema({
 
 app.get('/', function(req, res){
   // la funcion graphql permite realizar consultas al schema
-  graphql(schema, ` { message } `).then(r => res.json(r)).catch(res.json);
+  graphql(schema, ` { message, course{ title } } `).then(r => res.json(r)).catch(res.json);
 })
 
 app.listen(8080, function(){
