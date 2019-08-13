cae37b7359c2e171a68a5d93e3cb4729cf7e67c4
Jose Tafur <j-osea-nt@hotmail.com>
2019-08-12 19:05:16 -0500

    adding graphql server

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const express = require('express');
+const { GraphQLSchema, GraphQLObject, GraphQLString } = require('graphql');
 
 const app = express();
+
+// definir el schema
+// la funcion resolve indica como se debe responder cuando se solicite la prop message
+const schema = new GraphQLSchema({
+  query: new GraphQLObject({
+    name: 'RootQueryType',
+    fields: {
+      message: {
+        type: GraphQLString,
+        resolve(){
+          return "Hola"
+        }
+      }
+    }
+  })
+})
 
 app.get('/', function(req, res){
   res.send('Hola');
