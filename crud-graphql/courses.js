a9c90f73021b42db71e5db4379e62fc0a3f2b190
Jose Tafur <j-osea-nt@hotmail.com>
2019-10-18 20:37:12 -0500

    adding mock data

---
+++ crud-graphql/courses.js
@@ -0,0 +1,5 @@
+module.exports = [
+  {  id: "1", title: 'Course of GraphQL', views: 1000 },
+  {  id: "2", title: 'Course of Javascript', views: 50000 },
+  {  id: "3", title: 'Course of Angular', views: 75000 },
+]
--- crud-graphql/server.js
+++ crud-graphql/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 // hay 2 formas de definir el schema, una es con objetos (como esta en index.js) y la ota manera es con el buildSchema
 const { buildSchema } = require('graphql');
 
+const courses = require('./courses');
+
 const app = express();
 
 // el tipo ID de graphql hace referencia a un identificador ùnico, validando en una misma lista no halla varios objetos q tengan el mismo id
@@ -20,7 +22,7 @@ const schema = buildSchema(`
 `);
 
 app.get('', function(req, res) {
-  res.send('Hola')
+  res.json(courses);
 });
 
 app.listen(8080, function() {
