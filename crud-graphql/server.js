a2810cdb8802d6c1c21874f19f8f99eeaa6cf290
Jose Tafur <j-osea-nt@hotmail.com>
2019-10-18 20:45:40 -0500

    adding schema route

--- crud-graphql/server.js
+++ crud-graphql/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 // hay 2 formas de definir el schema, una es con objetos (como esta en index.js) y la ota manera es con el buildSchema
 const { buildSchema } = require('graphql');
 
+const graphqlHTTP = require('express-graphql');
+
 const courses = require('./courses');
 
 const app = express();
@@ -9,6 +11,7 @@ const app = express();
 // el tipo ID de graphql hace referencia a un identificador ùnico, validando en una misma lista no halla varios objetos q tengan el mismo id
 // el signo ! hace referencia a q esos campos son obligatorios
 // el tipo Query son las consultas q se van hacer al servidor de graphql
+// schema definition language
 const schema = buildSchema(`
   type Course {
     id: ID!
@@ -21,9 +24,23 @@ const schema = buildSchema(`
   }
 `);
 
+// tiene q tener el mismo nombre de la consulta en el tipo query para poder realizar bien los resolve
+const root = {
+  getCourses(){
+    return courses;
+  }
+}
+
 app.get('', function(req, res) {
   res.json(courses);
 });
+
+// graphiql es una interfaz gráfica para poder realizar consultas de graphql al servidor de graphql
+app.use('/graphql', graphqlHTTP({
+    schema,
+    rootValue: root,
+    graphiql: true
+}));
 
 app.listen(8080, function() {
   console.log('RUN SERVER');
